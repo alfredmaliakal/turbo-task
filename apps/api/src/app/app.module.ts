eabb45b8e0ee0a19eb5108839a0331bf59e0be5b
Alfred Antony <115711100+mkalfred@users.noreply.github.com>
2025-05-07 02:53:51 -0400

    added todo and typeorm

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule} from '@nestjs/config'
+import { TodosModule } from './todos/todos.module';
+import { Todo } from './todos/entities/todo.entity';
+import { path } from 'path';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'data/demo.db',
+      entities: [path.join(process.cwd(),'dist/**/*.entity.js')],
+      synchronize: true,
+    }),
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
